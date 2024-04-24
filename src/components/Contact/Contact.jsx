948d6f20c0b73999477c425b90015b66f96cea3f
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2024-04-24 01:57:27 -0400

    123e

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,35 +1,44 @@
 import { useDispatch } from "react-redux";
-// import { deleteContact } from "../../redux/contactsSlice";
 import css from "./Contact.module.css";
-import toast from "react-hot-toast";
+import { FaUser } from "react-icons/fa6";
+import { FaPhoneAlt } from "react-icons/fa";
 import { deleteContact } from "../../redux/contacts/operations";
 
-export default function Contact({ contact: { id, name, number } }) {
+export default function Contact({ info }) {
   const dispatch = useDispatch();
 
-  const handleDeleteContact = (id) => {
-    dispatch(deleteContact(id))
-      .unwrap()
-      .then(() => {
-        toast.success(`Contact ${name} was deleted!`);
-      })
-      .catch(() => {
-        toast.error("Something went wrong..");
-      });
-  };
+  // const handleDeleteContact = (id) => {
+  //   dispatch(deleteContact(id))
+  //     .unwrap()
+  //     .then(() => {
+  //       toast.success(`Contact ${name} was deleted!`);
+  //     })
+  //     .catch(() => {
+  //       toast.error("Something went wrong..");
+  //     });
+  // };
+
 
   return (
-    <li className={css.contacts_list__item}>
-      {name}:
-      <span>
-        <i>{number}</i>
-      </span>
-      <button
-        type="button"
-        className={css.contacts_button}
-        onClick={() => handleDeleteContact(id)}>
-        delete
-      </button>
-    </li>
+    <div className={css.box}>
+      <div>
+        <p className={css.string}>
+          <FaUser className={css.icon} />
+          {info.name}
+        </p>
+        <p className={css.string}>
+          <FaPhoneAlt className={css.icon} />
+          {info.number}
+        </p>
+      </div>
+      <Button
+        variant="contained"
+        className={css.button}
+        onClick={() => dispatch(deleteContact(info.id))}
+      >
+        Delete
+      </Button>
+    </div>
   );
+    
 }
